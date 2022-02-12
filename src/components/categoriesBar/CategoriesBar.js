e2de2e46d2a522d938c04d546d8bd7ecad1b74e3
m-eminozdogan <m.eminozdogan@hotmail.com>
2022-02-12 21:48:12 +0300

    categories bar style done

--- src/components/categoriesBar/CategoriesBar.js
+++ src/components/categoriesBar/CategoriesBar.js
@@ -1,8 +1,46 @@
-import React from 'react';
-import "./_categoriesBar.scss"
+import React, { useState } from "react";
+import "./_categoriesBar.scss";
+
+const keywords = [
+  "All",
+  "Not at all",
+  "Random",
+  "Bored",
+  "Funny",
+  "Science",
+  "Oldschool",
+  "G-Funk",
+  "Cypher",
+  "Jazz-rap",
+  "İstanbul",
+  "Flow",
+  "Mountain",
+  "Gaming",
+  "DIY",
+  "React",
+  "CSS",
+  "Mobile",
+  "Responsive",
+  "University"
+];
 function CategoriesBar() {
-  return <div>
-      cat bar
-  </div>;
+  const [activeElement, setActiveElement] = useState("All");
+  const handleClick = (value) => {
+    setActiveElement(value);
+  };
+
+  return (
+    <div className="categoriesBar">
+      {keywords.map((value, index) => (
+        <span
+          onClick={() => handleClick(value)}
+          className={activeElement === value ? "active" : ""}
+          key={index}
+        >
+          {value}
+        </span>
+      ))}
+    </div>
+  );
 }
 export default CategoriesBar;
